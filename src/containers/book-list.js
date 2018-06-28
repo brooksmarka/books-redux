d4faacce4fa3910dd4980fbe3b4ec0c047403abc
Mark Brooks <Markabrooks@gmail.com>
2018-06-28 17:38:33 -0600

    added a book detail view and selectedBook action creator wired to redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectBook } from "../actions/index";
+import { bindActionCreators } from "redux"; //takes return value from select book and makes sure itflows through reducers
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item"
+        >
           {book.title}
         </li>
       );
@@ -18,9 +24,21 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
+  //Whatever is returned will show up as props in BookList
   return {
-    books: state.books //this is the connection between redux and your container along with connect
+    books: state.books //this is returning an object with key of books assigning it from reducer
   };
 }
 
-export default connect(mapStateToProps)(BookList); //connect takes a function and component and produces a contianer
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  //whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch); //dispatch receives actions and spits them out to all the different reducers
+}
+
+//Promote BookList from a component to a container - it needs to know about this new dispatch method,
+//selectBook.  Make it available as a prop.
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookList); //connect takes a function and component and produces a contianer
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    { title: "The Dark Tower" },
-    { title: "The Monkeywrench Gang" },
-    { title: "Dharma Bums" },
-    { title: "Desert Solitaire" },
-    { title: "Freedom of the Hills" }
+    { title: "The Dark Tower", pages: 534 },
+    { title: "The Monkeywrench Gang", pages: 243 },
+    { title: "Dharma Bums", pages: 220 },
+    { title: "Desert Solitaire", pages: 123 },
+    { title: "Freedom of the Hills", pages: 501 }
   ];
 }
